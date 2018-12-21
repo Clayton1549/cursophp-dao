82e48180915f12254ccbdc95bcdc06d37d36cb17
Clayton Pereira de Oliveira <pereira.cleiton.oliver@gmail.com>
2018-12-21 18:56:56 -0200

    testando metodos no index php

--- index.php
+++ index.php
@@ -10,10 +10,40 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios ");
 echo json_encode($usuarios);*/
 
 
-$user = new Usuario();
+//   carrega um usuario 
+/*$user = new Usuario();
 
 $user->loadbyId(4); 
- echo $user;
+ echo $user;*/
+
+
+
+ //careega uma lista de usuario
+
+ /*$lista = Usuario::getList();
+
+ echo json_encode($lista);*/
+
+
+
+
+ //carrega uma lista de usuario buscando pelo login
+
+
+ /*$search = Usuario::search("Ro");
+ echo json_encode($search);*/
+
+
+//carrega um usuario usando ologin e a senha
+
+ $usuario = new Usuario();
+ $usuario->login("Roberto","00002446556");
+
+ echo $usuario;
+
+
+
+
 
 
 
